973235aed9d6969ee4c43a82b2c34a3252d55c1e
jamesdanielwhitford <james.whitford0@gmail.com>
2023-04-20 20:54:04 +0200

    Add 3D models to public folder

--- src/App.js
+++ src/App.js
@@ -2,7 +2,11 @@ import React, { useState } from 'react';
 import CardList from './CardList';
 
 const App = () => {
-  const [cards, setCards] = useState([]);
+  const [cards, setCards] = useState([
+    { title: 'Model 1', modelPath: 'myModel1.stl' },
+    { title: 'Model 2', modelPath: 'myModel2.stl' },
+    { title: 'Model 3', modelPath: 'myModel3.stl' },
+  ]);
 
   const handleAddCard = () => {
     const newCard = { content: `Card ${cards.length + 1}` };
